e10353aa977e7fd89554bfc2bd619fb5483c9b22
nagizero <nagi.zero+github@gmail.com>
2015-01-07 14:29:43 +0900

    add image tag converter

--- functions.php
+++ functions.php
@@ -1,3 +1,12 @@
 <?php
+
 add_theme_support('automatic-feed-links');
+
+add_filter('image_send_to_editor', 'img_tag_convert');
+function img_tag_convert($html) {
+    $html = "<figure>\n". $html. "\n<figcaption></figcaption>\n</figure>";
+    $class = 'img-responsive';
+    return str_replace('class="', 'class="'. $class. ' ', $html);
+}
+
 ?>
